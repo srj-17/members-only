346ef7a76869aed267d22d0cdf4618fa9203b469
srj-17 <saugatrijal17@gmail.com>
2025-04-15 17:59:07 +0545

    fix: Done show the same message again and again in login failure

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,14 +1,15 @@
 const passport = require("passport");
 
 function getLogin(req, res, next) {
-  const messages = req.session.messages;
+  // only show the first message if there are messages
+  const messages = req.session.messages ? req.session.messages.slice(0, 1) : "";
   res.render("login_form", { title: "Log in", messages: messages });
 }
 
 postLogin = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/log-in",
-  failureMessage: "Wrong username or password!",
+  failureMessage: "Wrong username or password",
 });
 
 module.exports = {
