cda9c58071b44d7bab67c4851d99e73b1ccd3a68
srj-17 <saugatrijal17@gmail.com>
2025-04-15 18:06:44 +0545

    fix: Add validation to login form password confirmation
    Users could easily log in without confirmation password
    matching the original password.
    
    Now they can't.

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,17 @@
 const passport = require("passport");
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long."),
+
+  body("confirm_password")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords must match."),
+];
 
 function getLogin(req, res, next) {
   // only show the first message if there are messages
@@ -6,11 +19,27 @@ function getLogin(req, res, next) {
   res.render("login_form", { title: "Log in", messages: messages });
 }
 
-postLogin = passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/log-in",
-  failureMessage: "Wrong username or password",
-});
+postLogin = [
+  validateUser,
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("login_form", {
+        title: "login_form",
+        errors: errors.array(),
+      });
+    }
+
+    // if there are no errors during validation
+    next();
+  },
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+    failureMessage: "Wrong username or password",
+  }),
+];
 
 module.exports = {
   getLogin,
